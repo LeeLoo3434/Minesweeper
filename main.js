ba0c8c52043f3a543cd8be44633022c4eb65123e
Lola Russell <113068944+LeeLoo3434@users.noreply.github.com>
2023-01-03 09:16:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+/*----- constants -----*/
+//board tiles- 10 x 10 - maybe loop through an array
+// tiles with div tags located by the row and column 
+const board = [];
+const rows = 10;
+const colums = 10;
+/*----- app's state (variables) -----*/
+// mine count + location 
+// cells clicked
+let cellsClicked = 0;
+let gameOver = false;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
